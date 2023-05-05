d527059960c9a9f873a1bff19c0fa5325b1acca3
Anton Golub <antongolub@antongolub.com>
2023-05-05 17:49:23 +0300

    chore: refactor file reader

--- src/main/ts/sourcecrumbs.ts
+++ src/main/ts/sourcecrumbs.ts
@@ -7,6 +7,12 @@ import fs from 'fs-extra'
 import smv from 'sourcemap-validator'
 import {globby} from 'globby'
 import {temporaryDirectory} from 'tempy'
+import parseUrl from 'parse-url'
+
+export type TPackument = {
+  name: string
+  version: string
+}
 
 export type TVerifyOptions = {
   name: string,
@@ -14,47 +20,133 @@ export type TVerifyOptions = {
   registry?: string
 }
 
-export const verifyPkg = async ({
-  name,
-  version,
-  registry
-}: TVerifyOptions) => {
-  const cwd = await fetchPkg({name, version, registry})
-  const files = await getPackageFiles(cwd)
-  const results = await verifyFiles(files)
+type TRawAttestation = {
+  predicateType: string
+  bundle: {
+    mediaType: string
+    verificationMaterial: any
+    dsseEnvelope: {
+      payload: string
+      payloadType: string
+      signatures: Array<{
+        sig: string
+        keyid: string
+      }>
+    }
+  }
+  [index: string]: any
+}
 
-  return files.reduce<Record<string, any>>((m, file, i) => {
-    m[path.relative(cwd, file)] = results[i]
+type TAttestation = {
+  context: any
+  raw: TRawAttestation
+}
+
+export type TRepository = {
+  type: string
+  url: string
+  hash?: string
+}
+
+export type TVerifyDigest = {
+  attestations: {
+    publish?: TAttestation
+    provenance?: TAttestation
+  }
+  repository: TRepository
+  contents: Record<string, any>
+}
+
+const normalizeRepoUrl = (url: string) => {
+  const opts = parseUrl(url)
+  return `git@${opts.resource}:${opts.pathname.slice(1)}`
+}
+
+export const verifyPkg = async ({
+                                  name,
+                                  version,
+                                  registry = 'https://registry.npmjs.org'
+                                }: TVerifyOptions) => {
+  const packument = await fetchPackument({name, version, registry})
+  const repository: TRepository = {...packument.repository, hash: packument.gitHead}
+  const targets = await fetchPkg({name, version, registry})
+  const sources = await fetchSources(repository)
+  const attestations = await getAttestation({name, version, registry})
+  const entries = verifyFiles(targets, sources)
+  // const results = await verifyFiles(files)
+
+  return {
+    entries
+  }
+
+  // const contents = files.reduce<Record<string, any>>((m, file, i) => {
+  //   m[path.relative(pkgDir, file)] = results[i]
+  //   return m
+  // }, {})
+
+  // return {
+  //   repository,
+  //   attestations,
+  //   contents,
+  //   packument
+  // }
+}
+
+const extractPayload = (v: string): any => JSON.parse(Buffer.from(v, 'base64').toString('utf8'))
+
+export const getAttestation = async ({name, version, registry}: any) => {
+  const raw: { attestations: TRawAttestation[] } = await fetchAttestation({name, version, registry})
+  const predicateTypesAliasMap: Record<string, string> = {
+    'https://slsa.dev/provenance/v0.2': 'provenance',
+    'https://github.com/npm/attestation/tree/main/specs/publish/v0.1': 'publish'
+  }
+
+  return raw.attestations.reduce((m: Record<string, TAttestation>, v) => {
+    m[predicateTypesAliasMap[v.predicateType]] = {
+      context: extractPayload(v.bundle.dsseEnvelope.payload),
+      raw: v
+    }
+
     return m
   }, {})
+}
+
+export const fetchSources = async ({url, hash = 'HEAD'}: TRepository) => {
+  const cwd = await fetchCommit({repo: normalizeRepoUrl(url), commit: hash})
+  return getFiles(cwd)
 }
 
 export const getPackumentUrl = (registry: string, name: string, version: string) => `${registry}/${name}/${version}`
 
 export const getTarballUrl = (registry: string, name: string, version: string) => `${registry}/${name}/-/${name.replace(/^.+(%2f|\/)/, '')}-${version}.tgz`
 
-export const fetchProvenance = async({name, registry, version}: any) =>
+export const fetchAttestation = async ({name, registry, version}: any) =>
   (await fetch(`${registry}/-/npm/v1/attestations/${name}@${version}`)).json()
 
-export const fetchPackument = async({name, version, registry}: any) =>
+export const fetchPackument = async ({name, version, registry}: any) =>
   (await fetch(getPackumentUrl(registry, name, version))).json()
 
-export const fetchCommit = async ({repo, commit, cwd = temporaryDirectory()}: {repo: string, commit: string, cwd?: string}) =>
+export const fetchCommit = async ({repo, commit, cwd = temporaryDirectory()}: {
+  repo: string,
+  commit: string,
+  cwd?: string
+}): Promise<string> =>
   new Promise((resolve, reject) => {
-    const child = cp.spawn('git', [`fetch --depth=1 ${repo} ${commit}`], {
-      cwd
+    const child = cp.spawn(`git clone -n --depth=1 ${repo} ${cwd} && git checkout ${commit}`, {
+      cwd,
+      shell: true,
+      // stdio: 'inherit'
     })
-
     child.on('error', reject)
     child.on('close', () => resolve(cwd))
   })
 
 export const fetchPkg = async ({
-  name,
-  version,
-  registry = 'https://registry.npmjs.org',
-  cwd = temporaryDirectory()
-}: any): Promise<string> => {
+                                 name,
+                                 version,
+                                 registry = 'https://registry.npmjs.org',
+                                 cwd = temporaryDirectory()
+                               }: any) => {
   const id = `${name}@${version}`
 
   try {
@@ -90,15 +182,15 @@ export const fetchPkg = async ({
     throw e
   }
 
-  return cwd
+  return getFiles(cwd)
 }
 
-export const validateSourcemap = (minifiedCode: string, sourceMap?: string | null): boolean => {
+export const validateSourcemap = (minifiedCode: string, sourceMap?: string | null, sources?: Record<string, string>): boolean => {
   if (!sourceMap) {
     return false
   }
   try {
-    smv(minifiedCode, sourceMap)
+    smv(minifiedCode, sourceMap, sources)
     return true
   } catch (e) {
     console.error(e)
@@ -107,18 +199,41 @@ export const validateSourcemap = (minifiedCode: string, sourceMap?: string | nul
   return false
 }
 
-export const verifyFiles = async (files: string[]) =>
-  Promise.all(files.map(async (file) => {
-    const contents = await fs.readFile(file, 'utf8')
-    const mapFile = `${file}.map`
-    const mapContents = await fs.exists(mapFile) ? await fs.readFile(mapFile, 'utf8') : null
+export const verifyFiles = (targets: Record<string, string>, sources?: Record<string, string>) => {
+  const findMatch = (contents: string, file: string, sources: Record<string, string> = {}) =>
+    sources[file] === contents ? file : Object.keys(sources).find(key => sources[key] === contents) || null
 
-    return {
-      sourcemap: validateSourcemap(contents, mapContents)
+  // const sources
+
+  return Object.entries(targets).reduce((m, [file, contents]) => {
+    const sm = targets[`${file}.map`]
+    const smdata = sm ? JSON.parse(sm) : null
+    const match = findMatch(contents, file, sources || {})
+
+    m[file] = {
+      match,
+      sourcemap: sm ? {
+        sources: smdata,
+        valid: validateSourcemap(contents, targets[`${file}.map`], sources)
+      } : null
     }
-  }))
 
-export const getPackageFiles = (cwd: string) => globby(['**/*'],{cwd, onlyFiles: true, absolute: true})
+    return m
+  },
+  {} as Record<string, any>)
+}
+
+export const getFiles = async (cwd: string, absolute = false) => {
+  const files = await globby(['**/*'], {cwd, onlyFiles: true, absolute})
+  const contents = await Promise.all(files.map((file) => fs.readFile(path.join(cwd, file), 'utf8')))
+
+  return files.reduce<Record<string, string>>((m, file, i) => {
+    m[file] = contents[i]
+    return m
+  }, {})
+}
+
+export const getPackageFiles = (cwd: string) => globby(['**/*'], {cwd, onlyFiles: true, absolute: true})
 
 // https://nodejs.org/api/packages.html
 // https://webpack.js.org/guides/package-exports/
--- src/test/ts/sourcecrumbs.ts
+++ src/test/ts/sourcecrumbs.ts
@@ -1,48 +1,48 @@
 import  { expect } from 'earljs'
 import { test } from 'uvu'
 
-import {fetchCommit, fetchPkg, verifyPkg, fetchPackument, fetchProvenance} from '../../main/ts/sourcecrumbs'
+import {fetchCommit, fetchPkg, verifyPkg, fetchPackument, fetchAttestation} from '../../main/ts/sourcecrumbs'
 
-// test('verifyPkg ', async () => {
-//   expect(await verifyPkg({name: 'toposource', version: '1.1.4'})).toEqual({
-//
-//   })
-// })
+test('verifyPkg ', async () => {
+  expect(await verifyPkg({name: 'toposource', version: '1.1.4'})).toEqual({
 
-test('verifySourcemap', async () => {
-  //  curl https://registry.npmjs.com/toposource > src/test/fixtures/toposource-packument.json
-  //
-})
-
-test('fetchPkg', async () => {
-  expect(await fetchPkg({name: 'toposource', version: '1.1.4'})).toBeA(String)
-})
-
-test('fetchCommit', async () => {
-  expect(await fetchCommit({
-    repo: 'git@github.com:semrel-extra/toposource.git',
-    commit: 'e9beae36161a8d44ffa072c2a58321d50af87919'
-  })).toBeA(String)
-})
-
-test('fetchPackument', async() => {
-  expect(await fetchPackument({name: 'toposource', version: '1.1.4', registry: 'https://registry.npmjs.org'})).toEqual(expect.objectWith({
-    name: 'toposource',
-    gitHead: 'e9beae36161a8d44ffa072c2a58321d50af87919'
-  }))
-})
-
-test('fetchProvenance', async () => {
-  expect(await fetchProvenance({name: 'toposource', version: '1.1.4', registry: 'https://registry.npmjs.org'})).toEqual({
-    attestations: [
-      expect.objectWith({
-        predicateType: 'https://slsa.dev/provenance/v0.2'
-      }),
-      expect.objectWith({
-        predicateType: 'https://github.com/npm/attestation/tree/main/specs/publish/v0.1'
-      })
-    ]
   })
 })
+
+// test('verifySourcemap', async () => {
+//   //  curl https://registry.npmjs.com/toposource > src/test/fixtures/toposource-packument.json
+//   //
+// })
+//
+// test('fetchPkg', async () => {
+//   expect(await fetchPkg({name: 'toposource', version: '1.1.4'})).toBeA(String)
+// })
+//
+// test('fetchCommit', async () => {
+//   expect(await fetchCommit({
+//     repo: 'git@github.com:semrel-extra/toposource.git',
+//     commit: 'e9beae36161a8d44ffa072c2a58321d50af87919'
+//   })).toBeA(String)
+// })
+//
+// test('fetchPackument', async() => {
+//   expect(await fetchPackument({name: 'toposource', version: '1.1.4', registry: 'https://registry.npmjs.org'})).toEqual(expect.objectWith({
+//     name: 'toposource',
+//     gitHead: 'e9beae36161a8d44ffa072c2a58321d50af87919'
+//   }))
+// })
+//
+// test('fetchAttestation', async () => {
+//   expect(await fetchAttestation({name: 'toposource', version: '1.1.4', registry: 'https://registry.npmjs.org'})).toEqual({
+//     attestations: [
+//       expect.objectWith({
+//         predicateType: 'https://slsa.dev/provenance/v0.2'
+//       }),
+//       expect.objectWith({
+//         predicateType: 'https://github.com/npm/attestation/tree/main/specs/publish/v0.1'
+//       })
+//     ]
+//   })
+// })
 
 test.run()
